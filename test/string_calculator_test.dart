5cdf33c402ab84086df5aed2928a5de161a67013
kevalvavaliya <kevalvavaliya@gmail.com>
2025-08-31 11:50:05 +0530

    red: add test for adding comma seperated numbers

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -3,11 +3,14 @@ import 'package:test/test.dart';
 import '../bin/string_calculator.dart';
 
 void main() {
-
   // Initial Testcase
   test('empty string returns 0', () {
     final calc = StringCalculator();
     expect(calc.add(''), equals(0));
   });
 
+  test('Sum of comma-separated numbers', () {
+    final calc = StringCalculator();
+    expect(calc.add("1,2,3"), equals(6));
+  });
 }
